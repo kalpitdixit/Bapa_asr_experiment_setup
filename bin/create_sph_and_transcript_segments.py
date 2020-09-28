19198f0d47587df02bb97166361fd8c415f48d7c
Ubuntu <ubuntu@ip-172-31-5-31.ec2.internal>
2020-09-28 01:15:21 +0000

    initial commit

---
+++ bin/create_sph_and_transcript_segments.py
@@ -0,0 +1,148 @@
+import os
+import glob
+import docx
+import re
+import hashlib
+import uuid
+import json
+from constants import DATA_DIR, KALDI_DIR
+
+
+sph2pipe = os.path.join(KALDI_DIR, "tools/sph2pipe_v2.5/sph2pipe")
+time_pattern = re.compile("\d\d:\d\d:\d\d", re.ASCII)
+punctuations = [".", ",", "!", "?"]
+
+
+def get_hash(s):
+    h = hashlib.sha1()
+    h.update(s.encode("utf-8"))
+    return h.hexdigest()
+
+
+def convert_to_seconds(s):
+    assert len(s)==8, "expected len(s) to be 8, but got len {}: {}".format(len(s), s)
+    assert time_pattern.match(s)
+    return float(s[0:2])*3600 + float(s[3:5])*60 + float(s[6:8])
+
+
+def empty_dir(dname):
+    if DATA_DIR in dname:
+        raise ValueError("trying to delete a data folder")
+    cmd = "rm -rf '{}'".format(dname)
+    os.system(cmd)
+
+
+def get_transcript_segments(fname):
+    transcript_segments = [] # list. each element is (start_time, end_time, text, uid). times are in seconds.
+    pattern = re.compile("\d\d:\d\d:\d\d - \d\d:\d\d:\d\d", re.ASCII)
+
+    doc = docx.Document(fname)
+    for para in doc.paragraphs:
+        text = para.text
+        text = text.strip()
+        if text=="":
+            continue
+        if not pattern.match(text[:19]):
+            continue
+        start_time = convert_to_seconds(text[:8])
+        end_time   = convert_to_seconds(text[11:19])
+        transcript = text[19:]
+        for punct in punctuations:
+            transcript = transcript.replace(punct, "")
+        if transcript=="":
+            continue
+        transcript = transcript.strip()
+
+        # see reason for not using global speaker ID by searching for "bold" in http://kaldi-asr.org/doc/data_prep.html
+        utt_id = str(uuid.uuid4())[:8]
+        uid = "{}-{}-{}".format(get_hash(transcript)[:8], "speaker1", str(uuid.uuid4())[:8])
+        #uid = "{}-{}-{}".format(get_hash(transcript)[:8], utt_id, utt_id)
+
+        transcript_segments.append((start_time, end_time, transcript, uid))
+    return transcript_segments
+    
+    
+def write_segmented_sphs(inp_sph_fname, transcript_segments, out_sph_dname):
+    for i,seg in enumerate(transcript_segments):
+        stime, etime, _, uid = seg[:]
+            
+        #out_sph_fname = os.path.basename(inp_sph_fname).split(".sph")[0] + "-seg-{}".format(i+1) + ".sph"
+        #out_sph_fname = os.path.join(out_sph_dname, out_sph_fname)
+        out_sph_fname = os.path.join(out_sph_dname, "{}.sph".format(uid))
+        cmd = "{} -f sph -t {}:{} '{}' '{}'".format(sph2pipe, stime, etime, inp_sph_fname, out_sph_fname)
+        os.system(cmd)
+        #os.system("ls -l '{}' | wc -l".format(os.path.dirname(out_sph_fname)))
+
+
+def write_segmented_transcripts(transcript_segments, out_transcript_dname):
+    with open(os.path.join(out_transcript_dname, "all.transcriptions"), "a") as fw:
+        for i,seg in enumerate(transcript_segments):
+            _, _, transcript, uid = seg[:]
+            out_str = "<s> {} </s> ({})".format(transcript, uid)
+            fw.write(out_str+"\n")
+
+
+def read_mapping_file(data_dir):
+    out_map = {}
+    with open(os.path.join(data_dir, "audios_sph_transcripts_map.json"), "r") as f:
+        for line in f:
+            x = json.loads(line)
+            out_map[x["audio_sph_file"]] = x["transcript_file"]
+    return out_map
+
+
+
+if __name__=="__main__":
+    ##### ARGUMENTS #####
+    #inp_sph_fname        = sys.argv[1] # /home/ubuntu/data/paryushan-2019/audios-sph/Day 1 - Pravachan 1 - 26th August 2019
+    #inp_transcripts_fname = sys.argv[2] # Pr1-Part 1-PTK522-PARYUSHAN2019-26082019-Atm. Sonalben-10092019.docx
+
+    #inp_dname = "Day 2 - Pravachan 2 - 27th August 2019"
+    #inp_dname = "Day 1 - Pravachan 1 - 26th August 2019"
+
+    inp_dnames = ["Day 1 - Pravachan 1 - 26th August 2019",
+                  "Day 2 - Pravachan 2 - 27th August 2019",
+                  "Day 3 - Pravachan 3 - 28th August 2019",
+                  "Day 4 - Pravachan 4 - 29th August 2019",
+                  "Day 6 - Pravachan 5 - 31st August 2019",
+                  "Day 7 - Pravachan 6 - 1st September 2019",
+                  "Day 8 - Pravachan 7 - 2nd September 2019"]
+    data_dir = "../../../data/paryushan-2019"
+    
+    audios_sph_to_transcript_map = read_mapping_file(data_dir)
+
+    for inp_dname in inp_dnames:
+        inp_sph_dname        = os.path.join(data_dir, "audios-sph/{}".format(inp_dname))
+        inp_transcript_dname = os.path.join(data_dir, "transcripts/{}".format(inp_dname))
+        
+        out_sph_dname        = os.path.join(data_dir, "segmented-audio-sph/{}".format(inp_dname))
+        out_transcript_dname = os.path.join(data_dir, "segmented-transcripts/{}".format(inp_dname))
+
+        ##### SETUP #####
+        for dname in [out_sph_dname, out_transcript_dname]:
+            empty_dir(dname)
+            if not os.path.exists(dname):
+                os.makedirs(dname)
+
+        ##### RUN #####
+        inp_sph_fnames        = glob.glob(os.path.join(inp_sph_dname, "*"))
+        inp_transcript_fnames = glob.glob(os.path.join(inp_transcript_dname, "*docx"))
+        inp_transcript_fnames = dict([(os.path.join(os.path.basename(os.path.dirname(os.path.dirname(x))),
+                                                                                     os.path.basename(os.path.dirname(x)),
+                                                                                     os.path.basename(x)), x)
+                                      for x in inp_transcript_fnames])
+        for inp_sph_fname in inp_sph_fnames:
+            key = os.path.join(os.path.basename(os.path.dirname(os.path.dirname(inp_sph_fname))),
+                                                                os.path.basename(os.path.dirname(inp_sph_fname)),
+                                                                os.path.basename(inp_sph_fname))
+            if not key in audios_sph_to_transcript_map:
+                print("no transcript file for: {}".format(key))
+                continue
+            inp_transcript_fname = audios_sph_to_transcript_map[key]
+            inp_transcript_fname = inp_transcript_fnames[inp_transcript_fname]
+
+            transcript_segments = get_transcript_segments(inp_transcript_fname) # list. each elem is (start_time, end_time, text). times are in seconds.
+            write_segmented_sphs(inp_sph_fname, transcript_segments, out_sph_dname)
+            write_segmented_transcripts(transcript_segments, out_transcript_dname)
+        
+
---
+++ bin/setup_train_run_2.py
@@ -0,0 +1,124 @@
+import os
+import glob
+from constants import DATA_DIR, ESPNET_DIR
+import argparse
+
+
+
+def empty_dir(dname):
+    if DATA_DIR in dname:
+        raise ValueError("trying to delete a data folder")
+    cmd = "rm -rf {}".format(dname)
+    os.system(cmd)
+
+
+def copy_from_dir_to_dir(source_dir, dest_dir, config, file_list_fname=None):
+    os.system("mkdir -p {}".format(dest_dir))
+    if file_list_fname is not None:
+        os.system("mkdir -p {}".format(os.path.dirname(file_list_fname)))
+        fw = open(file_list_fname, "a")  
+    count = 0
+    for fname in sorted(glob.glob(os.path.join(source_dir, "*"))):
+        cmd = "cp '{}' '{}'".format(fname, os.path.join(dest_dir, os.path.basename(fname)))
+        os.system(cmd)
+    
+        if file_list_fname is not None:
+            fw.write("{} {} -f wav -p -c 1 {} |\n".format(os.path.basename(fname).replace(".sph", ""),
+                                                          os.path.join(ESPNET_DIR, "tools/kaldi/tools/sph2pipe_v2.5/sph2pipe"),
+                                                          os.path.join("downloads/Bapa/wav/an4{}_clstk/speaker1".format("test" if "an4test" in dest_dir else ""), os.path.basename(fname))))
+        count += 1
+        if config.tiny_train and count>=8:
+            break
+
+    if file_list_fname is not None:
+        fw.close()
+
+
+def copy_from_dir_to_file(source_dir, dest_file, allowed_ids, create_text_file=False):
+    os.system("mkdir -p {}".format(os.path.dirname(dest_file)))
+
+    with open(dest_file, "a") as fw:
+        for fname in sorted(glob.glob(os.path.join(source_dir, "*"))):
+            with open(fname, "r") as f:
+                for line in f:
+                    if allowed_ids is not None:
+                        line_id = line.strip().split()[-1][1:-1]
+                        if not line_id in allowed_ids:
+                            continue
+                    if create_text_file:
+                        line = line.strip().split()
+                        line = "{} {}\n".format(line[-1][1:-1], " ".join(line[1:-2]))
+                    fw.write(line)
+                        
+
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tiny_train", action="store_true")
+    return parser.parse_args()
+
+
+if __name__=="__main__":
+    ##### ARGUMENTS #####
+    config = parser()
+    #if config.tiny_train:
+    #    raise NotImplementedError("not correctly implemented")
+
+    DATA_DIR = os.path.join(DATA_DIR, "paryushan-2019")
+    train_dirs = ["Day 1 - Pravachan 1 - 26th August 2019",
+                  #"Day 2 - Pravachan 2 - 27th August 2019", # marked as text
+                  "Day 3 - Pravachan 3 - 28th August 2019",
+                  "Day 4 - Pravachan 4 - 29th August 2019",
+                  "Day 6 - Pravachan 5 - 31st August 2019",
+                  "Day 7 - Pravachan 6 - 1st September 2019"]
+    train_audio_dirs = []
+    train_transcript_dirs = []
+    for tdir in train_dirs:
+        train_audio_dirs.append(os.path.join(DATA_DIR, "segmented-audio-sph/{}".format(tdir)))
+        train_transcript_dirs.append(os.path.join(DATA_DIR, "segmented-transcripts/{}".format(tdir)))
+
+    test_audio_dirs      = [os.path.join(DATA_DIR, "segmented-audio-sph/Day 2 - Pravachan 2 - 27th August 2019")]
+    test_transcript_dirs = [os.path.join(DATA_DIR, "segmented-transcripts/Day 2 - Pravachan 2 - 27th August 2019")]
+
+    ##### RUN #####
+    DEST_DIR = os.path.join(ESPNET_DIR, "egs/Bapa/asr1")
+    empty_dir(os.path.join(DEST_DIR, "downloads"))
+    empty_dir(os.path.join(DEST_DIR, "data"))
+    os.system("rm -rf {}".format(os.path.join(DEST_DIR, "data", "train", "wav.scp")))
+    os.system("rm -rf {}".format(os.path.join(DEST_DIR, "data", "test", "wav.scp")))
+
+    ### TRAIN
+    for dname in train_audio_dirs:
+        copy_from_dir_to_dir(dname, os.path.join(DEST_DIR, "downloads", "Bapa", "wav", "an4_clstk", "speaker1"), config, 
+                             file_list_fname=os.path.join(DEST_DIR, "data", "train", "wav.scp"))
+
+    allowed_ids = None
+    if config.tiny_train:
+        fname = os.path.join(DEST_DIR, "data", "train", "wav.scp")
+        with open(fname, "r") as f:
+            allowed_ids = [line.strip().split()[0] for line in f]
+
+    for dname in train_transcript_dirs:
+        copy_from_dir_to_file(dname, os.path.join(DEST_DIR, "data", "train", "text"), allowed_ids, create_text_file=True)
+        copy_from_dir_to_file(dname, os.path.join(DEST_DIR, "downloads", "Bapa", "etc", "train.transcription"), allowed_ids)
+
+
+    ### TEST
+    for dname in test_audio_dirs:
+        copy_from_dir_to_dir(dname, os.path.join(DEST_DIR, "downloads", "Bapa", "wav", "an4test_clstk", "speaker1"), config,
+                             file_list_fname=os.path.join(DEST_DIR, "data", "test", "wav.scp"))
+
+    allowed_ids = None
+    if config.tiny_train:
+        fname = os.path.join(DEST_DIR, "data", "test", "wav.scp")
+        with open(fname, "r") as f:
+            allowed_ids = [line.strip().split()[0] for line in f]
+
+    for dname in test_transcript_dirs:
+        copy_from_dir_to_file(dname, os.path.join(DEST_DIR, "data", "test", "text"), allowed_ids, create_text_file=True)
+        copy_from_dir_to_file(dname, os.path.join(DEST_DIR, "downloads", "Bapa", "etc", "test.transcription"), allowed_ids)
+
+
+    ### add README
+    with open(os.path.join(DEST_DIR, "downloads", "Bapa", "README"), "w") as f:
+        f.write("tmp\n")
+
