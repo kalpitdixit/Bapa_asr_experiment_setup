0700b168867537be34ae9d263d53191cc1e40306
kalpitdixit <kalpitdixit@gmail.com>
2021-05-17 02:26:22 +0000

    updating folder structure

--- bin/constants.py
+++
@@ -1,3 +0,0 @@
-DATA_DIR="/home/ubuntu/data"
-ESPNET_DIR="/home/ubuntu/code/espnet"
-KALDI_DIR="/home/ubuntu/code/kaldi"
--- bin/utils.py
+++
@@ -1,6 +0,0 @@
-import json
-
-
-def read_jsonl_file(fname):
-    with open(fname, "r") as f:
-        return [json.loads(line) for line in f]
