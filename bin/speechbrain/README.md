4f8ee586bc3dfc577d872157b6b0562d56bb43b1
Kalpit Dixit <kalpitdixit@gmail.com>
2021-06-08 08:58:14 +0000

    updates

--- bin/speechbrain/README.md
+++ bin/speechbrain/README.md
@@ -1,13 +1,10 @@
 # SETUP
-## Install SeepchBrain
-`git clone https://github.com/speechbrain/speechbrain.git`  
-Commit ID: 9382406e8e8b42753ac317bf5b2ef1a91ddfa9c3
+* install specific library versions from `requirements.txt`
 
 ## Standard ASR Process
-see [this](https://github.com/speechbrain/speechbrain/tree/develop/recipes/LibriSpeech) for the need to train Tokenizer first and only then AM and LM.
+see [this (LibriSpeech recipe)](https://github.com/speechbrain/speechbrain/tree/develop/recipes/LibriSpeech) for the need to train Tokenizer first and only then AM and LM.
 
-# DESIGN PRINCIPLES
-1. we use [HyperPyYAML](`https://github.com/speechbrain/HyperPyYAML#yaml-basics`) for all kinds of hyperparam communication
+# Design Principles
+1. we use [HyperPyYAML](`https://github.com/speechbrain/HyperPyYAML#yaml-basics`) for all hyperparameter communication
 
 
-1. datasets are communicated between data-store/cli and speechbrain interface by compiling manifest files of individual utterances; jsonl
--- bin/speechbrain/utils.py
+++ bin/speechbrain/utils.py
@@ -1,6 +1,6 @@
 import os
 import json
-from hyperpyyaml import load_hyperpyyaml
+from hyperpyyaml import load_hyperpyyaml, dump_hyperpyyaml
 
 
 
@@ -15,19 +15,44 @@ def write_jsonl_file(fname, lines):
             fw.write(json.dumps(line, sort_keys=True)+"\n")
 
 
-def get_utterance_manifest_from_data_config(data_config):
+def load_hparams(hparams_fname):
+    with open(hparams_fname, "r") as f:
+        return load_hyperpyyaml(f)
+
+
+def combine_multiple_hyperpyyaml_files_into_one(input_hyperpyyaml_files=None, extra_kv={}, output_hyperpyyaml_file=None):
+    """
+    # input_hyperpyyaml_files: dictionary of hyperpyyaml config files for this experiment
+    # output_hyperpyyaml_file: str
+    """
+    combined_hparams = {}
+    for k,v in input_hyperpyyaml_files.items():
+        with open(v, "r") as f:
+            combined_hparams[k] = load_hyperpyyaml(f)
+
+    for k,v in extra_kv.items():
+        combined_hparams[k] = v
+
+    write_hyperpyyaml_file(output_hyperpyyaml_file, combined_hparams)
+
+
+def write_hyperpyyaml_file(out_fname, hparams):    
+    print(hparams)
+    with open(out_fname, "w") as fw:
+        dump_hyperpyyaml(hparams, fw)
+
+
+def get_utterance_manifest_from_datasets(datasets):
     """
     INPUT:
-        data_config in hyperpyyaml format
+        datasets: the datasets field from a data_config hyperpyyaml file
 
     OUTPUT:
-        list of entries from .jsonl files. [{"audo_sph_file": ..., "transcript_file": ..., "filter_criteria": ...}, ...]
+        list of entries from .jsonl files. [{"audo_wav_file": ..., "transcript_all_file": ..., "filter_criteria": ...}, ...]
     """
-    # Datasets
-    with open(data_config, "r") as f:
-        datasets = load_hyperpyyaml(f)["datasets"]
-
-    # Convert to utterance manifest
+    ### Convert to utterance manifest
+    # at this stage, the trancsript info for each utterance is a filename with multiple trancsripts ('transcript_all_file')
+    # and an uid into that file, `transcript_uid`
     corpus = []
     for ds in datasets:
         entries = [entry for entry in read_jsonl_file(ds["map_file"]) if entry["filter_criteria"] in ds["filter_criterias"]]
@@ -38,5 +63,31 @@ def get_utterance_manifest_from_data_config(data_config):
             entry["transcript_all_file"] = os.path.join(root_dir, entry["transcript_all_file"])
             corpus.append(entry)
 
-    return corpus
+    ### Get list of transcript_all files and assert that they exist
+    # beginning the effort to directly have the trnascripts in each entry instead of transcript_files and uids
+    unique_transcript_all_files = list(set([entry["transcript_all_file"] for entry in corpus]))
+    for x in unique_transcript_all_files:
+        assert os.path.exists(x), "data transcript file {} does not exist! Exiting!".format(x)
 
+    ### Get Transcript Map
+    # key = transcript uid
+    # value = transcript line
+    # <s> અંબાલાલ ભાઈને ચાર વાર ભલામણ કરી છે ત્રિભોવનભાઇ ને બે વાર ભલામણ કરી છે </s> (b0d740f0-speaker1-00f44130)
+    transcript_map = {}
+    for transcript_all_file in unique_transcript_all_files:
+        with open(transcript_all_file, "r") as f:
+            for line in f:
+                key = line.split("</s>")[-1].strip()[1:-1]
+
+                line = line.strip().split()
+                assert line[0]=="<s>"
+                assert line[-2]=="</s>"
+                transcript = " ".join(line[1:-2])
+
+                transcript_map[key] = transcript
+
+    for entry in corpus:
+        assert entry["transcript_uid"] in transcript_map
+        entry["transcript"] = transcript_map[entry["transcript_uid"]]
+    
+    return corpus
