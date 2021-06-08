f3d1ee59fa39f9303e0ac9f4b5275124d9f382a0
Kalpit Dixit <kalpitdixit@gmail.com>
2021-06-08 09:24:07 +0000

    added recording of unk_index, bos_index, eos_index and pad_index

--- bin/speechbrain/launch_tokenizer_train.py
+++ bin/speechbrain/launch_tokenizer_train.py
@@ -24,7 +24,13 @@ def main(config):
     write_hyperpyyaml_file(os.path.join(config.output_folder, "sp_vocab_{}_{}.yaml".format(config.vocab_size, config.model_type)),
                            {"model_dir": config.output_folder,
                             "vocab_size": config.vocab_size,
-                            "model_type": config.model_type})
+                            "model_type": config.model_type,
+                            "sp_model_file": os.path.join(config.output_folder, "{}_{}.model".format(str(config.vocab_size), config.model_type)),
+                            "unk_index": config.unk_index,
+                            "bos_index": config.bos_index,
+                            "eos_index": config.eos_index,
+                            "pad_index": config.pad_index})
+
 
 
     ### train custom SentencePiece Tokenizer ###
@@ -37,10 +43,10 @@ def main(config):
                       annotation_train         = f.name,
                       annotation_read          = annotation_read,
                       annotation_format        = "json",
-                      unk_id                   = 0,
-                      bos_id                   = 1,
-                      eos_id                   = 2,
-                      pad_id                   = 3,
+                      unk_id                   = config.unk_index,
+                      bos_id                   = config.bos_index,
+                      eos_id                   = config.eos_index,
+                      pad_id                   = config.pad_index,
                       model_type               = config.model_type,
                       character_coverage       = config.character_coverage,
                       annotation_list_to_check = config.annotation_list_to_check)
@@ -61,6 +67,12 @@ def argparser():
                                                                 "and 1.0 for other languages with small character set.")
     parser.add_argument("--annotation_list_to_check", type=str, nargs="+", required=True, help="List of the annotation file which is used for checking "
                                                                                                "the accuracy of recovering words from the tokenizer.")
+
+    ## SPECIAL TOKENS ##
+    parser.add_argument("--unk_index", type=int, default=0) 
+    parser.add_argument("--bos_index", type=int, default=1) 
+    parser.add_argument("--eos_index", type=int, default=2) 
+    parser.add_argument("--pad_index", type=int, default=3) 
     return parser.parse_args()
 
 
